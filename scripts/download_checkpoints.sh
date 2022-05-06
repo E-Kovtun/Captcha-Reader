4abb2683fe8f426623066791e4dbad91668a12df
E-Kovtun <lizakovtun1003@mail.ru>
2022-05-06 23:56:33 +0300

    fix scripts

---
+++ scripts/download_checkpoints.sh
@@ -0,0 +1,2 @@
+gdown --fuzzy https://drive.google.com/file/d/1JVHRzyjMxazVme3RpMsHCjHzTd5xgxDo/view?usp=sharing
+unzip results.zip
--- scripts/download_init_dataset.sh
+++ scripts/download_init_dataset.sh
@@ -1,3 +1,3 @@
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/file/d/1uZdE8d4fTV-tUMrYMTyfaCxmGb6CiE_p/view?usp=sharing' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1uZdE8d4fTV-tUMrYMTyfaCxmGb6CiE_p" -O '../dataset.tar.xz' && rm -rf /tmp/cookies.txt
 cd ../
+gdown --fuzzy https://drive.google.com/file/d/1uZdE8d4fTV-tUMrYMTyfaCxmGb6CiE_p/view?usp=sharing
 tar -xvf dataset.tar.xz
